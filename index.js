3b976cc8726f53d0da95fb8d4632d00882d96d0c
Mabe-Diaby <diabymabe@gmail.com>
2021-12-07 10:51:26 -0500

    initial project scaffolded

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+const arr = [
+    { name: 'Will'},
+    { name: 'Bill'}
+];
+var result = arr.map(e => e.name)
+console.log(result);
+
+var body = document.querySelector('body')
